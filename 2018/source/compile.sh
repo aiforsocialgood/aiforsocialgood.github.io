ab00ed833846c12c1d7e093be1d4d34bdd9d23a8
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-11-07 22:50:00 -0500

    update sponsors and proposals

--- 2018/source/compile.sh
+++ 2018/source/compile.sh
@@ -2,3 +2,4 @@ cat header home footer > ../index.htm
 cat header cfp footer > ../cfp.htm
 cat header schedule footer > ../schedule.htm
 cat header organizers footer > ../organizers.htm
+cat header proposals footer > ../proposals.htm
