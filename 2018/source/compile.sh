f593e1ee85b007ebb3c758ca8178d661803993c3
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-11-20 16:13:12 -0500

    add accepted papers

--- 2018/source/compile.sh
+++ 2018/source/compile.sh
@@ -3,3 +3,4 @@ cat header cfp footer > ../cfp.htm
 cat header schedule footer > ../schedule.htm
 cat header organizers footer > ../organizers.htm
 cat header proposals footer > ../proposals.htm
+cat header acceptedpapers footer > ../acceptedpapers.htm
