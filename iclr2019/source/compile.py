31b57d509f4d248ca40a7ec928f1ac277c115736
Genevieve Boucher <6923093+genevievebo@users.noreply.github.com>
2019-02-17 11:31:53 -0500

    Moving all possible python code from template to compile.py

--- iclr2019/source/compile.py
+++ iclr2019/source/compile.py
@@ -1,3 +1,4 @@
+import os
 import re
 import json
 
@@ -5,6 +6,45 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 reg = "\$\{[^$]*\}"
+
+
+def getAcceptedPapers(papers_dir, papers_pdf_link):
+    '''
+    Retrieve the list of accepted papers from 
+    directory defined in the .json file
+
+    Name of the .txt file should contain the id
+    '''
+    papers = [] 
+    filenames = os.listdir(papers_dir)
+    filenames = [f for f in filenames if '.txt' in f]
+    
+    for filename in filenames:
+        id = filename.split('_')[0]
+        name = filename.split('.')[0]
+
+        finput = open('{}/{}'.format(papers_dir, filename))
+        lines = finput.readlines()
+        infos = dict([tuple(l.strip().split(':')) for l in lines])
+        
+        # information not in the file
+        if "pdf" not in infos.keys():
+            infos["pdf"] = '{}/{}.pdf'.format(papers_pdf_link, name)
+
+        infos["id"] = id
+        infos["name"] = name
+
+        # Check that pdf exists
+        rootdir = os.path.dirname(os.getcwd())
+        pdffile = os.path.join(rootdir, infos['pdf'])
+        if not os.path.exists(pdffile):
+            infos["pdf"] = ""
+        papers.append(infos)
+    return papers
+
+def getScheduleItems():
+    # see schedule template
+    pass
 
 mylookup = TemplateLookup(directories=['.', '../bio'],
                           strict_undefined=False,
@@ -18,6 +58,7 @@ sections = ['proposals', 'home', 'schedule', 'faq',
             'acceptedpapers', 'cfp', 'organizers']
 
 for section_name in sections:
+
     attributes = custom['default'].copy()
     attributes.update(custom.get(section_name, {}))
 
@@ -28,15 +69,26 @@ for section_name in sections:
     section = Template(html, lookup=mylookup, strict_undefined=False)
 
     try:
+
+        # Retrieve accepted papers from directory
+        if section_name == 'acceptedpapers':
+            papers_dir = attributes['papers_dir']
+            papers_pdf_link = attributes['papers_pdf_link']
+            papers = getAcceptedPapers(papers_dir, papers_pdf_link)
+            attributes['papers'] = papers
+
+        # Generating the different .htm files
         s = section.render(**attributes)
         outfile = open('../%s.htm' % section_name, "w")
         outfile.write(str(s))
         outfile.close()
 
+        # Copy home as index
         if section_name == 'home':
             outfile = open('../index.htm', "w")
             outfile.write(str(s))
             outfile.close()
+
 
     except Exception as e:
         print(section_name, e)
