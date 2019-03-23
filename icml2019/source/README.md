2868461745e0f14e29ada0ad2a61b04804b2b59d
Museau <margaux.luck@gmail.com>
2019-03-23 23:37:15 +0100

    add AISGICMLW website

---
+++ icml2019/source/README.md
@@ -0,0 +1,63 @@
+## ICML AISG 2019 worskhop website
+
+To compile the website :
+`python compile.py`
+
+The python script uses mako templating package to create the html pages.
+Mako templates allow the usage of python inside the html files which allows for dynamic generation of html.
+
+Two mechanisms are used :
+
+* Workshop custom informations are defined in the `custom.json` file and values are retrieved to insert
+information inside the different pages.
+
+* Information is retrieved from files found in the directory structure.  Files are read (see code in the templates)
+and the html is generated automatically.  This is used for the schedule and the accepted papers.
+
+
+### Schedule
+
+Schedule items are defined in the custom.json file (time and speaker name).  Since writing the title and abstract in
+the json file is not user-friendly, these are read from a different file (one per schedule item).
+
+The directory `schedule-items`, contains a file per speaker (event) with the required information.
+
+For example, in `custom.json`  :
+```
+ {"time" : "9:00", "speaker_name": "Speaker Name"}
+```
+
+in `Spearker_Name.txt` (name needs to be identical to the name entered in the .json)
+```
+title:Opening remarks
+abstract:
+```
+
+The template will look for all the files corresponding to the speaker name and will create an entry for each.
+
+### Accepted papers
+
+The accepted papers are retrieved from the`papers_dir` defined in the `custom.json` file.
+Here, the different elements are found from the files present in the specified  directory.
+
+In the current structure, they are found in the `accepted/track1`.
+
+This directory contains a `pdfs` folder and files containing informations about each accepted paper.
+
+For example, `0_aisg_icml2019.txt`:
+
+```
+title:Example
+authors:People Any, And Other, et. al
+tag:
+```
+
+The pdf file can be specified or by default, is assumed to match the name of the .txt file (0_aisg_icml2019.pdf).
+
+The template retrieves all the .txt files in the directory and creates a table dynamically containing the specified information.
+
+
+#### Requirements :
+* python v3
+* mako
+
---
+++ icml2019/source/compile.py
@@ -0,0 +1,108 @@
+import os
+import re
+import json
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+reg = "\$\{[^$]*\}"
+
+
+def getAcceptedPapers(papers_dir, papers_pdf_link):
+    '''
+    Retrieve the list of accepted papers from
+    directory defined in the .json file
+
+    Name of the .txt file should contain the id
+    '''
+    papers = []
+    filenames = os.listdir(papers_dir)
+    filenames = [f for f in filenames if '.txt' in f]
+
+    for filename in filenames:
+        id = filename.split('_')[0]
+        name = filename.split('.')[0]
+
+        finput = open('{}/{}'.format(papers_dir, filename))
+        lines = finput.readlines()
+        infos = dict([tuple(l.strip().split(':')) for l in lines])
+
+        # information not in the file
+        if "pdf" not in infos.keys():
+            infos["pdf"] = '{}/{}.pdf'.format(papers_pdf_link, name)
+
+        infos["id"] = id
+        infos["name"] = name
+
+        # Check that pdf exists
+        rootdir = os.path.dirname(os.getcwd())
+        pdffile = os.path.join(rootdir, infos['pdf'])
+        if not os.path.exists(pdffile):
+            infos["pdf"] = ""
+        papers.append(infos)
+    return papers
+
+
+def getScheduleItems():
+    # see schedule template
+    pass
+
+mylookup = TemplateLookup(directories=['.', '../bio', '..'],
+                          strict_undefined=False,
+                          input_encoding='utf-8')
+
+# Load custom attributes from json file
+custom = json.loads(open('custom.json').read())
+
+# Get all pages
+pages = ['proposals', 'home', 'schedule', 
+            'acceptedpapers', 'cfp', 'organizers',
+            'guidelines_areachairs', 'guidelines_reviewers', 
+            'pastworkshops',
+            'faq_general', 'faq_reviewers', 'faq_fundings',
+            'faq_submission'
+            ]
+
+for page_name in pages:
+
+    attributes = custom['default'].copy()
+    attributes.update(custom.get(page_name, {}))
+
+    html = """<%include file="header"/>
+              <%include file="{}"/>
+              <%include file="footer"/>""".format(page_name)
+
+    page = Template(html, lookup=mylookup, strict_undefined=False)
+
+    try:
+
+        # Retrieve accepted papers from directory
+        if page_name == 'acceptedpapers':
+            papers_dir = attributes['papers_dir']
+            papers_pdf_link = attributes['papers_pdf_link']
+            papers = getAcceptedPapers(papers_dir, papers_pdf_link)
+            attributes['papers'] = papers
+
+        # Generating the different .htm files
+        s = page.render(**attributes)
+        outfile = open('../%s.htm' % page_name, "w")
+        outfile.write(str(s))
+        outfile.close()
+
+        # Copy home as index
+        if page_name == 'home':
+            outfile = open('../index.htm', "w")
+            outfile.write(str(s))
+            outfile.close()
+
+    except Exception as e:
+        print(page_name, e)
+
+        ctc = re.findall(reg, open(page_name).read())
+        atr = attributes.keys()
+        for c in ctc:
+            c = c.strip("${}")
+            if c not in atr:
+                print(page_name, " : ", c,  " is missing")
+        # print("footer", re.findall(reg, open("footer").read()))
+        # print("header", re.findall(reg, open("header").read()))
