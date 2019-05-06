c9f8438eac1ba83f63febc508415164ff7866f9f
Museau <Museau@users.noreply.github.com>
2019-05-06 11:10:18 -0400

    Modify schedule / add tags

--- iclr2019/source/compile.py
+++ iclr2019/source/compile.py
@@ -12,7 +12,6 @@ def getAcceptedPapers(papers_dir, papers_pdf_link, posters_pdf_link):
     '''
     Retrieve the list of accepted papers from
     directory defined in the .json file
-
     Name of the .txt file should contain the id
     '''
     papers = []
@@ -26,7 +25,7 @@ def getAcceptedPapers(papers_dir, papers_pdf_link, posters_pdf_link):
         finput = open('{}/{}'.format(papers_dir, filename))
         lines = finput.readlines()
         # all files need to have the same number of elements
-        
+
         infos = dict([tuple(l.strip().split(':')) for l in lines])
 
         # information not in the file
@@ -45,9 +44,9 @@ def getAcceptedPapers(papers_dir, papers_pdf_link, posters_pdf_link):
         infos["poster"] = insertIfExists(rootdir, infos["poster"])
 
         papers.append(infos)
-        
+
     papers =  sorted(papers, key=lambda k: int(k['id']))
-    papers =  sorted(papers, key=lambda k: k.get('category', '')) 
+    papers =  sorted(papers, key=lambda k: k.get('category', ''))
     return papers
 
 
@@ -111,7 +110,7 @@ for page_name in pages:
             outfile = open('../index.htm', "w")
             outfile.write(str(s))
             outfile.close()
-    
+
     except Exception as e:
         print(page_name, e)
 
